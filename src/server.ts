406944693ba39dd8bdcf73d9834a1f87f6898031
Henrique Calenzo <calenzodevs@gmail.com>
2021-06-24 17:12:58 -0300

    Class 02
    .gitignore
    	Adicionado o .vscode
    readme
    	Adicionado regras
    package.json
    	Atualizado as dependencias
    ormconfig
    	Adicionado typeorm e sqlite3
    src/database
    	Adicionado a tabela usuários
    src/controller/CreateUserController.ts
    	Responsavel por retornar o JSON user
    src/entities/User.ts
    	Entidade User
    src/repositories/UsersRepositories.ts
    	Acessando repositorio atraves da entidade
    src/router.ts
    	Acessando as rotas de controller
    src/server.ts
    	Removido os comentarios desnecessários
    src/services/CreateUserService.ts
    	Acessando a reqição de user e fazendo as verificações

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,15 @@
+import "reflect-metadata";
 import express from "express";
+
+import { router } from "./router";
+
+import "./database";
 
 const app = express();
 
-/*
-GET     => Buscar alguma informação
-POST    => Inserir/Criar
-PUT     => Alterar
-DELETE  => Remover
-PATCH   => Alterar algo especificamente
-*/
+app.use(express.json())
+app.use(router)
 
-app.get("/test", (require, response) => {
-    return response.send("App na NLW");
-})
-
-app.post("/test-post", (require, response) => {
-    return response.send("App na NLW");
-})
-
-//http://localhost:3000/
 app.listen(3000, () => {
     console.log("Server is running");
 });
